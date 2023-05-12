987a1b7fb9586755776b9ced327a9f5c0294f750
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-05-12 17:46:52 +0530

    added bunch of stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 
 const todos:string[] = ["Meet Chad"];
+import { revalidatePath } from 'next/cache';
 import Image from 'next/image'
 export default function Home() {
+  async function addTodo(data:FormData)
+  {
+    'use server';
+    const todo = data.get("todo") as string ; 
+    todos.push(todo)
+    revalidatePath("/");
+  }
   return (
     <main className="p-24">  
     <h1 className='text-4xl font-bold'>Todos</h1>
@@ -10,10 +18,13 @@ export default function Home() {
         <li key={index}>{todo}</li>
       ))}
     </ul>
-    <form>
+    <form action = {addTodo}>
       <input type="text"
       name="todo"
       className='border border-gray-300 rounded-lg py-4 px-4 text-base'/>
+      <button type="submit" className='bg-blue-600 disabled:bg-gray-500 inline-flex items center '>
+        Add Todo
+      </button>
 
     </form>
     
